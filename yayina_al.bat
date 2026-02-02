59b3fd14f17b5c9b654997473c7d7e12a78abf15
Metehan Harita <sinanmete@hotmail.com.tr>
2026-02-03 01:14:12 +0300

    Otomatik guncelleme: 03.02.2026  1:14:12.47

--- yayina_al.bat
+++ yayina_al.bat
@@ -1,17 +1,47 @@
 @echo off
-echo ----------------------------------------
-echo METGEO360 OTOMATIK GUNCELLEME SISTEMI
-echo ----------------------------------------
+color 0A
+echo ================================================
+echo   METGEO360 OTOMASYON ROBOTU BASLATILIYOR
+echo ================================================
 echo.
-echo 1. Klasorler taraniyor ve harita verileri olusturuluyor...
+
+echo [1/3] Python veri taramasi basliyor...
 python site_guncelle.py
+IF %ERRORLEVEL% NEQ 0 (
+    color 0C
+    echo.
+    echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    echo   HATA: Python scripti calisirken sorun cikti!
+    echo   Lutfen yukaridaki hata mesajini okuyun.
+    echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    pause
+    exit /b
+)
+echo ... Veriler basariyla hazirlandi.
 echo.
-echo 2. Degisiklikler GitHub'a gonderiliyor...
+
+echo [2/3] Degisiklikler GitHub'a paketleniyor...
 git add .
-git commit -m "Yeni proje eklendi ve harita guncellendi"
-git push
+git commit -m "Otomatik guncelleme: %date% %time%"
 echo.
-echo ----------------------------------------
-echo ISLEM TAMAM! Siteniz 1 dakika icinde guncellenecek.
-echo ----------------------------------------
+
+echo [3/3] GitHub Sunucusuna Gonderiliyor...
+git push
+IF %ERRORLEVEL% NEQ 0 (
+    color 0C
+    echo.
+    echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    echo   HATA: Dosyalar GitHub'a GONDERILEMEDI!
+    echo   Internet baglantini veya GitHub girisini kontrol et.
+    echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    pause
+    exit /b
+)
+
+color 0A
+echo.
+echo ================================================
+echo   ISLEM BASARIYLA TAMAMLANDI!
+echo   Siteniz 1-2 dakika icinde guncellenecektir.
+echo ================================================
 pause
